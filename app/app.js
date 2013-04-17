16f5318da1a394372cf0c4a937da64992c3dec8c
edmarriner <edmarriner@gmail.com>
2013-04-17 14:50:57 +0100

    facebook coonect

--- app/app.js
+++ app/app.js
@@ -1,11 +1,46 @@
-$(document).ready(function(){
+document.addEventListener('deviceready', function() {
 
-	// show app view
 	if ((typeof cordova == 'undefined') && (typeof Cordova == 'undefined')) alert('Cordova variable does not exist. Check that you have included cordova.js correctly');
             if (typeof CDV == 'undefined') alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
             if (typeof FB == 'undefined') alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
+
+
+  try {
+  alert('Device is ready! Make sure you set your app_id below this alert.');
+  FB.init({ appId: "425907780826832", nativeInterface: CDV.FB, useCachedDialogs: false });
+  document.getElementById('data').innerHTML = "";
+  } catch (e) {
+  alert(e);
+  }
+  }, false);
+
+	FB.Event.subscribe('auth.login', function(response) {
+                               alert('auth.login event');
+                               });
             
-	
+            FB.Event.subscribe('auth.logout', function(response) {
+                               alert('auth.logout event');
+                               });
+            
+            FB.Event.subscribe('auth.sessionChange', function(response) {
+                               alert('auth.sessionChange event');
+                               });
+            
+            FB.Event.subscribe('auth.statusChange', function(response) {
+                               alert('auth.statusChange event');
+                               });
+
+
+            function getLoginStatus() {
+                FB.getLoginStatus(function(response) {
+                                  if (response.status == 'connected') {
+                                  alert('logged in');
+                                  } else {
+                                  alert('not logged in');
+                                  }
+                                  });
+            }
+
 	alert("init done");
 	// JQUERY Functions
 
