a849ac12ae5898c4d0dd8409329c92ebf5e06aee
edmarriner <edmarriner@gmail.com>
2013-04-18 00:24:40 +0100

    debug

--- app/app.js
+++ app/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+if ((typeof cordova == 'undefined') && (typeof Cordova == 'undefined')) alert('Cordova variable does not exist. Check that you have included cordova.js correctly');
+            if (typeof CDV == 'undefined') alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
+            if (typeof FB == 'undefined') alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
 
   try {
   alert('Device is ready! Make sure you set your app_id below this alert.');
