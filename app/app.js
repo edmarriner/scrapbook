2d67ff8d64c0d75b25f7cca2904b888421db35da
edmarriner <edmarriner@gmail.com>
2013-04-20 17:40:59 +0100

    facebook friends invite

--- app/app.js
+++ app/app.js
@@ -400,8 +400,8 @@ $(document).ready(function() {
 
 	    render: function()
 	    {
-	    	
-	    	 FB.api(
+
+	    	FB.api(
 		    {
         		method: 'fql.query',
         		query: 'SELECT uid , name, pic_square FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1'
