461a837458be61e98aa101bff770f09ba9908824
edmarriner <edmarriner@gmail.com>
2013-05-07 14:58:03 +0200

    moved into doc ready

--- app/app.js
+++ app/app.js
@@ -1,17 +1,17 @@
-document.addEventListener('deviceready', function() {
-	console.log("deviceReady");
-	if ((typeof cordova == 'undefined') && (typeof Cordova == 'undefined')) alert('Cordova variable does not exist. Check that you have included cordova.js correctly');
-	if (typeof CDV == 'undefined') alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
-	if (typeof FB == 'undefined') alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
-
-	// handshake the facebook API
-	try {
-		FB.init({ appId: "425907780826832", nativeInterface: CDV.FB, useCachedDialogs: false });
-	} catch (e) {
-		alert(e);
-	}
-
-}, false);
+//document.addEventListener('deviceready', function() {
+//	console.log("deviceReady");
+//	if ((typeof cordova == 'undefined') && (typeof Cordova == 'undefined')) alert('Cordova variable does not exist. Check that you have included cordova.js correctly');
+//	if (typeof CDV == 'undefined') alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
+//	if (typeof FB == 'undefined') alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
+//
+//	// handshake the facebook API
+//	try {
+//		FB.init({ appId: "425907780826832", nativeInterface: CDV.FB, useCachedDialogs: false });
+//	} catch (e) {
+//		alert(e);
+//	}
+//
+//}, false);
 
 
 // offline
@@ -20,17 +20,19 @@ document.addEventListener('deviceready', function() {
 
 $(document).ready(function() {
 	
+	alert("starting ...");
 	if ((typeof cordova == 'undefined') && (typeof Cordova == 'undefined')) alert('Cordova variable does not exist. Check that you have included cordova.js correctly');
 	if (typeof CDV == 'undefined') alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
 	if (typeof FB == 'undefined') alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
-
+	alert("all requirments okay...")
 	try {
+		alert("about to try init")
+		alert(FB)
 		FB.init({ appId: "425907780826832", nativeInterface: CDV.FB, useCachedDialogs: false });
 	} catch (e) {
 		alert(e);
 	}
-	
-	console.log("documentReady");
+	alert("done init");
 	//FB.Event.subscribe('auth.login', function(response) {
     //	alert('auth.login event'); 
    	//});
